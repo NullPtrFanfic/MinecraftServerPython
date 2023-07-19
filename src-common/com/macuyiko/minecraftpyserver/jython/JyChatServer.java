8e8992f24a22c6fd0b8d91bc2077ad2525e75965
NullPtrFanfic <nullptrlog@gmail.com>
2023-07-19 12:48:38 +0300

    Release

---
+++ src-common/com/macuyiko/minecraftpyserver/jython/JyChatServer.java
@@ -0,0 +1,110 @@
+package com.macuyiko.minecraftpyserver.jython;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.python.core.PyException;
+
+import com.macuyiko.minecraftpyserver.MinecraftPyServerPlugin;
+
+public class JyChatServer {
+	private MinecraftPyServerPlugin plugin;
+	private Map<String, JyInterpreter> players;
+	private Map<String, MyOutputStream> outstreams;
+	private Map<String, String> buffers;
+	
+	public JyChatServer(MinecraftPyServerPlugin caller) {
+		this.plugin = caller;
+		this.players = new HashMap<String, JyInterpreter>();
+		this.outstreams = new HashMap<String, MyOutputStream>();
+		this.buffers = new HashMap<String, String>();
+	}
+		
+	public void setupInterpreter(String player) {
+		JyInterpreter interpreter = new JyInterpreter();
+		MyOutputStream os = new MyOutputStream(this, player);
+		interpreter.setOut(os);
+		interpreter.setErr(os);
+		outstreams.put(player, os);
+		players.put(player, interpreter);
+		buffers.put(player, "");
+	}
+
+	public void command(String player, String message) {
+		if (!players.containsKey(player)) {
+			answer(player, "Starting Python... this can take a few seconds\n");
+			setupInterpreter(player);
+		}
+				
+		final JyInterpreter interpreter = players.get(player);
+		boolean more = false;
+		try {
+			if (message.contains("\n")) {
+				more = JyParser.parse(interpreter, message, true);
+			} else {
+				buffers.put(player, buffers.get(player)+"\n"+message); 
+				more = JyParser.parse(interpreter, buffers.get(player), false);
+			}
+		} catch (PyException e) {
+			answer(player, e.toString()+"\n");
+		}
+		outstreams.get(player).flush();
+		if (!more) {
+			interpreter.resetbuffer();
+			buffers.put(player, "");
+		}
+	}
+	
+	public void file(String player, File script) {
+		if (!players.containsKey(player)) {
+			answer(player, "Starting Python... this can take a few seconds\n");
+			setupInterpreter(player);
+		}
+		final JyInterpreter interpreter = players.get(player);
+		interpreter.resetbuffer();
+		try {
+			JyParser.parse(interpreter, script);
+		} catch (PyException e) {
+			answer(player, e.toString()+"\n");
+		}
+		outstreams.get(player).flush();
+		buffers.put(player, "");
+	}
+	
+	public void answer(String player, String message) {
+		plugin.send(player, message);
+	}
+
+	public class MyOutputStream extends OutputStream {
+		String player;
+		StringBuffer buffer = new StringBuffer("");
+		JyChatServer server;
+		public MyOutputStream(JyChatServer server, String player) {
+			this.player = player;
+			this.server = server;
+		}
+		@Override
+		public void write(int b) {
+			byte[] bytes = { (byte) b };
+			write(bytes, 0, bytes.length);
+		}
+		@Override
+		public void write(byte[] bytes, int offset, int length) {
+			String s = new String(bytes, offset, length);
+			buffer.append(s);
+			if (buffer.toString().endsWith("\n"))
+				this.flush();
+		}
+		@Override
+		public void flush() {
+			String sb = buffer.toString();
+			if (!sb.equals(""))
+				server.answer(player, buffer.toString());
+			buffer.delete(0, buffer.length());
+		}
+	}
+	
+
+
+}
---
+++ src-common/com/macuyiko/minecraftpyserver/jython/JyWebSocketServer.java
@@ -0,0 +1,136 @@
+package com.macuyiko.minecraftpyserver.jython;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.framing.CloseFrame;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.python.core.PyException;
+
+import com.macuyiko.minecraftpyserver.MinecraftPyServerPlugin;
+
+public class JyWebSocketServer extends WebSocketServer {
+	private MinecraftPyServerPlugin plugin;
+	private Map<WebSocket, JyInterpreter> connections;
+	private Map<WebSocket, MyOutputStream> outstreams;
+	private Map<WebSocket, String> buffers;
+	
+	public JyWebSocketServer(MinecraftPyServerPlugin caller, int port) {
+		super(new InetSocketAddress(port));
+		this.plugin = caller;
+		this.connections = new HashMap<WebSocket, JyInterpreter>();
+		this.outstreams = new HashMap<WebSocket, MyOutputStream>();
+		this.buffers = new HashMap<WebSocket, String>();
+	}
+	
+	public void cleanup() {
+		Set<Entry<WebSocket, JyInterpreter>> entries = connections.entrySet();
+		for (Entry<WebSocket, JyInterpreter> e : entries){
+			if (!connections.get(e.getKey()).isAlive()) {
+				plugin.log("Cleaning up an idle connection");
+				close(e.getKey());
+			}
+		}
+	}
+	
+	public void setupInterpreter(WebSocket ws) {
+		JyInterpreter interpreter = new JyInterpreter();
+		MyOutputStream os = new MyOutputStream(ws);
+		interpreter.setOut(os);
+		interpreter.setErr(os);
+		outstreams.put(ws, os);
+		connections.put(ws, interpreter);
+		buffers.put(ws, "");
+	}
+	
+	public void close(WebSocket ws) {
+		if (connections.containsKey(ws))
+			connections.get(ws).cleanAndClose();
+		connections.remove(ws);
+		buffers.remove(ws);
+		ws.close(0);
+	}
+
+	@Override
+	public void onOpen(WebSocket ws, ClientHandshake chs) {
+		plugin.log("New websocket connection");
+		cleanup();
+		plugin.log("Starting interpreter");
+		setupInterpreter(ws);
+	}
+
+	@Override
+	public void onClose(WebSocket ws, int arg1, String arg2, boolean arg3) {
+		close(ws);
+	}
+
+	@Override
+	public void onMessage(WebSocket ws, final String message) {
+		if (message.equals("!exit")) {
+			ws.close(CloseFrame.NORMAL);
+			return;
+		}
+		if (message.equals("!restart")) {
+			plugin.log("Restarting interpreter");
+			setupInterpreter(ws);
+			return;
+		}
+		
+		final JyInterpreter interpreter = connections.get(ws);
+		boolean more = false;
+		try {
+			if (message.contains("\n")) {
+				more = JyParser.parse(interpreter, message, true);
+			} else {
+				buffers.put(ws, buffers.get(ws)+"\n"+message); 
+				more = JyParser.parse(interpreter, buffers.get(ws), false);
+			}
+		} catch (PyException e) {
+			ws.send(e.toString()+"\n");
+		}
+		outstreams.get(ws).flush();
+		if (!more) {
+			interpreter.resetbuffer();
+			buffers.put(ws, "");
+		}
+		if (more) ws.send("... ");
+		else ws.send(">>> ");
+	}
+
+	@Override
+	public void onError(WebSocket ws, Exception exc) {
+		close(ws);
+	}
+
+	public class MyOutputStream extends OutputStream {
+		WebSocket ws;
+		StringBuffer buffer = new StringBuffer("");
+		public MyOutputStream(WebSocket ws) {
+			this.ws = ws;
+		}
+		@Override
+		public void write(int b) {
+			byte[] bytes = { (byte) b };
+			write(bytes, 0, bytes.length);
+		}
+		@Override
+		public void write(byte[] bytes, int offset, int length) {
+			String s = new String(bytes, offset, length);
+			buffer.append(s);
+			if (buffer.toString().endsWith("\n"))
+				this.flush();
+		}
+		@Override
+		public void flush() {
+			this.ws.send(buffer.toString());
+			buffer.delete(0, buffer.length());
+		}
+	}
+
+}
