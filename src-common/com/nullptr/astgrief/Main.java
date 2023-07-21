ed996b3cb1217e534a79f7e4a29be9e63d50f38d
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-21 12:05:54 +0300

    Update Main.java

--- src-common/com/nullptr/astgrief/Main.java
+++ src-common/com/nullptr/astgrief/Main.java
@@ -8,6 +8,9 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		log("Loading AstGrief plugin..");
+	        SERVER = Bukkit.getServer()
+                WORLD = SERVER.getWorlds().get(0)
+                PLUGIN = SERVER.getPluginManager().getPlugin('astgrief')
 	}
 	
 	public void log(String message) {
