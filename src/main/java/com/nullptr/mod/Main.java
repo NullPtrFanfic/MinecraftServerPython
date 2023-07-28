f3c7e59087baf4453c6d2cc6a08f45a5bcb6cea4
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-28 11:46:25 +0300

    Update Main.java

--- src/main/java/com/nullptr/mod/Main.java
+++ src/main/java/com/nullptr/mod/Main.java
@@ -1,1 +1,31 @@
+package com.nullptr.mod;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "mod";
+    public static final String NAME = "NullPtr Mod";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
