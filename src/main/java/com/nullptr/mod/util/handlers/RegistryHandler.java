16d485155e8516bedcdbc53ccc58d2ff480a8b0d
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-30 14:44:04 +0300

    Update RegistryHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/RegistryHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/RegistryHandler.java
@@ -1,82 +1,18 @@
 @EventBusSubscriber
+public class RegistryHandler {
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
+    }
 
-public class RegistryHandler 
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        for (Item item : ItemInit.ITEMS) {
+            if (item instanceof IHasModel) {
+                ((IHasModel)item).registerModels();
+            }
+        }
+    }
 
-{
-
-    @SubscribeEvent
-
-    public static void onItemRegister(RegistryEvent.Register<Item> event)
-
-    {
-
-        event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
-
-    }
-
-    
-
-    @SubscribeEvent
-
-    public static void onModelRegister(ModelRegistryEvent event)
-
-    {
-
-        for(Item item : ItemInit.ITEMS)
-
-        {
-
-            if(item instanceof IHasModel)
-
-            {
-
-                ((IHasModel)item).registerModels();
-
-            }
-
-        }
-
-    }
-
-    
-
-    public static void preInitRegistries()
-
-    {
-
-        
-
-    }
-
-    
-
-    public static void initRegistries()
-
-    {
-
-        
-
-    }
-
-    
-
-    public static void postInitRegistries()
-
-    {
-
-        
-
-    }
-
-    
-
-    public static void serverRegistries(FMLServerStartingEvent event)
-
-    {
-
-        
-
-    }
-
+    // Здесь могут быть другие методы, связанные с регистрацией предметов, блоков и других объектов
 }
-
