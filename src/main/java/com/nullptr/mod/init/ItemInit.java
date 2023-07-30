a8ba66839763a6b4ca57bff1a289afc70e74d631
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-30 12:15:59 +0300

    Create ItemInit.java

---
+++ src/main/java/com/nullptr/mod/init/ItemInit.java
@@ -0,0 +1,12 @@
+package com.nullptr.mod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+
+public class ItemInit
+{
+    public static final List<Item> ITEMS = new ArrayList<Item>();
+    public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
+}
