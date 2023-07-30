19a3f3f00e6f86a3a70992d450583fcaccd4be53
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-30 14:06:43 +0300

    Update CommonProxy.java

--- src/main/java/com/nullptr/mod/proxy/CommonProxy.java
+++ src/main/java/com/nullptr/mod/proxy/CommonProxy.java
@@ -1,18 +1,11 @@
 import net.minecraft.item.Item;
 
- 
-
 public class CommonProxy 
-
 {
-
     public void registerItemRenderer(Item item, int meta, String id) 
-
     {
 
         
-
     }
-
 }
 
