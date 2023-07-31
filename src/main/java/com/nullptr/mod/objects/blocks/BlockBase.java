b9ddff7f7c5221ebe32f4c5e995fd9a2986b65b4
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-31 14:46:19 +0300

    Update BlockBase.java

--- src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
@@ -1,7 +1,11 @@
 package com.nullptr.mod.objects.blocks;
 import com.nullptr.mod.util.interfaces.IHasModel;
+import com.nullptr.mod.Main;
+import com.nullptr.mod.init.BlockInit;
+import com.nullptr.mod.init.ItemInit;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.block.material.Material;
 public class BlockBase extends Block implements IHasModel
 {
@@ -13,5 +17,10 @@ public class BlockBase extends Block implements IHasModel
        setCreativeTab(CreativeTabs.MATERIALS);
        BlockInit.BLOCKS.add(this);
        ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+   }
+   @override
+   public void registerModels()
+   {
+      Mod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
    }
 }
