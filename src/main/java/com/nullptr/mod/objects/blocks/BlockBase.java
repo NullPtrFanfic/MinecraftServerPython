5734b3460ef1758f3ae232e531faae7f349e48fd
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-31 14:41:13 +0300

    Update BlockBase.java

--- src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
@@ -2,10 +2,16 @@ package com.nullptr.mod.objects.blocks;
 import com.nullptr.mod.util.interfaces.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
-public class BlockBase extends Block implements IHasMod
+import net.minecraft.block.material.Material;
+public class BlockBase extends Block implements IHasModel
 {
-   public BlockBase()
+   public BlockBase(String name, Material material)
    {
-       ItemInit.ITEMS.add(new ItemBlock(this.getRegistryName(name)))
+       super(material);
+       setUnlocalizedName(name);
+       setRegistryName(name);
+       setCreativeTab(CreativeTabs.MATERIALS);
+       BlockInit.BLOCKS.add(this);
+       ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
    }
 }
