3ee646a2549a553a98e09c685b62dd8ce1be3506
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-07-31 15:09:58 +0300

    Create Tab.java

---
+++ src/main/java/com/nullptr/mod/tabs/Tab.java
@@ -0,0 +1,16 @@
+package com.nullptr.mod.tabs;
+
+import com.nullptr.mod.init.BlockInit;
+import com.nullptr.mod.init.ItemInit;
+
+import net.minectaft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+public class Tab extends CreativeTabs
+{
+   public Tab(String label)
+   {
+      sup
+   }
+}
