5c81f65ce4756e9d0ecb1d37da6cbefd7de33118
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-01 15:39:30 +0300

    Update ItemInit.java

--- src/main/java/com/nullptr/mod/init/ItemInit.java
+++ src/main/java/com/nullptr/mod/init/ItemInit.java
@@ -16,7 +16,7 @@ public class ItemInit
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
     public static final Item OBSIDIAN_INGOT = new ItemBase("obsidian_ingot");
-    public static final ArmorMaterial ARMOR_RUBY = EnumHelper.addArmorMaterial("armor_ruby", Main.MOD_ID, + ":ruby", 1500, new int[] {4, 7, 9, 5}, 17, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
+    public static final ArmorMaterial ARMOR_RUBY = EnumHelper.addArmorMaterial("armor_ruby", Main.MODID + ":ruby", 1500, new int[] {4, 7, 9, 5}, 17, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0F);
     public static final Item HELMET_RUBY =  new ArmorBase("helmet_ruby", ARMOR_RUBY, 1, EntityEquipmentSlot.HEAD);
     public static final Item CHESTPLATE_RUBY =  new ArmorBase("chestplate_ruby", ARMOR_RUBY, 1, EntityEquipmentSlot.CHEST);
     public static final Item LEGGINGS_RUBY =  new ArmorBase("leggings_ruby", ARMOR_RUBY, 2, EntityEquipmentSlot.LEGS);
