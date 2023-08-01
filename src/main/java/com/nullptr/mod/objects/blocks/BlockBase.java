f4d1d8477ba61c725ab5eb76177ebd047993a034
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-01 10:27:47 +0300

    Update BlockBase.java

--- src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockBase.java
@@ -15,7 +15,7 @@ public class BlockBase extends Block implements IHasModel
        super(material);
        setUnlocalizedName(name);
        setRegistryName(name);
-       setCreativeTab(CreativeTabs.MATERIALS);
+       setCreativeTab(Main.MODTAB);
        BlockInit.BLOCKS.add(this);
        ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
    }
