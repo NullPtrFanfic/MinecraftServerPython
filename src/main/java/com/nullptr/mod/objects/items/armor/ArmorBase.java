b0b4d1785fe2a546cc915714c23cab90b3f80018
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-01 15:04:00 +0300

    Create ArmorBase.java

---
+++ src/main/java/com/nullptr/mod/objects/items/armor/ArmorBase.java
@@ -0,0 +1,23 @@
+package com.nullptr.mod.objects.items.armor;
+import com.nullptr.mod.Main;
+import net.minecraft.item.ItemArmor;
+import com.nullptr.mod.init.ItemInit;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import com.nullptr.mod.util.interfaces.IHasModel;
+public class ArmorBase extends ItemArmor implements IHasModel
+{
+   public ArmorBase(String name, ArmorMaterial material, int renderIndex, EntityEquipmentSlot equipmentSlot)
+   {
+      super(material, renderIndex, equipmentSlot);
+      setUnlocalizedName(name);
+      setRegistryName(name);
+      setCreativeTab(Main.MODTAB);
+
+      ItemInit.ITEMS.add(this);
+   }
+   @Override
+   public void registerModels()
+   {
+      Main.proxy.registerItemRenderer(this, 0, "inventory");
+   }
+}
