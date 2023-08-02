e97d413914677d2e04c46f3b9cff709c8dfe151c
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-02 11:42:44 +0300

    Update LargeFireballStaff.java

--- src/main/java/com/nullptr/mod/objects/items/staffs/LargeFireballStaff.java
+++ src/main/java/com/nullptr/mod/objects/items/staffs/LargeFireballStaff.java
@@ -3,15 +3,18 @@ package com.nullptr.mod.objects.items.staffs;
 import com.nullptr.mod.Main;
 import com.nullptr.mod.init.ItemInit;
 import com.nullptr.mod.util.interfaces.IHasModel;
+import net.minecraft.entity.projectile.EntityLargeFireball;
 import net.minecraft.item.Item;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 public class LargeFireballStaff extends Item implements IHasModel
 {
+   public int explosionPower;
    public LargeFireballStaff(String name, int explosionSize)
    {
       setUnlocalizedName(name);
@@ -25,7 +28,14 @@ public class LargeFireballStaff extends Item implements IHasModel
       ItemStack item = playerIn.getHeldItem(handIn);
       Vec3d look = playerIn.getLookVec();
       EntityLargeFireball largeFireball = new EntityLargeFireball(worldin, playerIn, 1D, 1D, 1D);
-     
+      largeFireball.setPosition(playerIn.posX + look.x * 1.5D, playerIn.posY + look.y * 1.5D, playerIn.posZ + look.z * 1.5D);
+      largeFireball.accelerationX = look.x * 7.0D;
+      largeFireball.accelerationY = look.y * 7.0D;
+      largeFireball.accelerationZ = look.z * 7.0D;
+      largeFireball.explosionPower = explosionPower;
+      largeFireball.setGlowing(true);
+      worldIn.spawnEntity(largeFireball);
+      return new ActionResult<Itemstack>(EnumActionResult.SUCCESS, item);
    }
    @Override
    public void registerModels()
