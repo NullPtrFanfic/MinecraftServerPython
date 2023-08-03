9549ff2c6cc29eaf0ce9edd64f2993a82d2f6e05
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-03 12:01:57 +0300

    Create EntityInit.java

---
+++ src/main/java/com/nullptr/mod/init/EntityInit.java
@@ -0,0 +1,21 @@
+package com.nullptr.mod.init;
+import com.nullptr.mod.Main;
+import net.minecraft.entity.Entity;
+import com.nullptr.mod.entity.test.EntityTest;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+public class EntityInit
+{
+   public static void registerEntities()
+   {
+      registerEntity("test", EntityTest.class, Main.ENTITY_TEST, 50, 13310623, 65354);
+   }
+   private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2)
+   {
+       EntityRegistry.registerModEntity(new ResourceLocation(Main.MODID + ":" + name), entity, name, id, Main.instance, range, 1, true, color1, color2);
+   }
+   private static void registerNonMobEntity()
+   {
+     
+   }
+}
