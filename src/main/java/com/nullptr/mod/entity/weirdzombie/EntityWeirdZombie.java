d31359041e32064e42f5ff62a6d94ce1d6c793c0
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 14:01:55 +0300

    Update EntityWeirdZombie.java

--- src/main/java/com/nullptr/mod/entity/weirdzombie/EntityWeirdZombie.java
+++ src/main/java/com/nullptr/mod/entity/weirdzombie/EntityWeirdZombie.java
@@ -1,34 +1,110 @@
-package com.nullptr.mod.entity.weirdzombie;
+package mcjty.modtut.entity;
 
-import com.nullptr.mod.entity.weirdzombie.EntityWeirdZombie;
-import com.nullptr.mod.entity.weirdzombie.RenderWeirdZombie;
-import com.nullptr.mod.Main;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.init.Biomes;
+import mcjty.modtut.ModTut;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.monster.EntityIronGolem;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTableList;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class EntityWeirdZombie()
-{
-    public static void init() {
-        // Every entity in our mod has an ID (local to this mod)
-        int id = 1;
-        EntityRegistry.registerModEntity(new ResourceLocation(ModTut.MODID, "weirdzombie"), EntityWeirdZombie.class, "WeirdZombie", id++, ModTut.instance, 64, 3, true, 0x996600, 0x00ff00);
+import javax.annotation.Nullable;
 
-        // We want our mob to spawn in Plains and ice plains biomes. If you don't add this then it will not spawn automatically
-        // but you can of course still make it spawn manually
-        EntityRegistry.addSpawn(EntityWeirdZombie.class, 100, 3, 5, EnumCreatureType.MONSTER, Biomes.PLAINS, Biomes.ICE_PLAINS);
+public class EntityWeirdZombie extends EntityMob {
 
-        // This is the loot table for our mob
-        LootTableList.register(EntityWeirdZombie.LOOT);
+    // We reuse the zombie model which has arms that need to be raised when the zombie is attacking:
+    private static final DataParameter<Boolean> ARMS_RAISED = EntityDataManager.createKey(EntityWeirdZombie.class, DataSerializers.BOOLEAN);
+
+    public static final ResourceLocation LOOT = new ResourceLocation(ModTut.MODID, "entities/weird_zombie");
+
+    public EntityWeirdZombie(World worldIn) {
+        super(worldIn);
+        setSize(0.6F, 1.95F);
+    }
+
+    @Override
+    protected void entityInit() {
+        super.entityInit();
+        this.getDataManager().register(ARMS_RAISED, Boolean.valueOf(false));
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        // Here we set various attributes for our mob. Like maximum health, armor, speed, ...
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.13D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(2.0D);
+    }
+
+   public void setArmsRaised(boolean armsRaised) {
+        this.getDataManager().set(ARMS_RAISED, Boolean.valueOf(armsRaised));
     }
 
     @SideOnly(Side.CLIENT)
-    public static void initModels() {
-        RenderingRegistry.registerEntityRenderingHandler(EntityWeirdZombie.class, RenderWeirdZombie.FACTORY);
+    public boolean isArmsRaised() {
+        return this.getDataManager().get(ARMS_RAISED).booleanValue();
+    }
+
+    @Override
+    protected void initEntityAI() {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(2, new EntityAIWeirdZombieAttack(this, 1.0D, false));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAILookIdle(this));
+        this.applyEntityAI();
+    }
+
+    private void applyEntityAI() {
+        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[]{EntityPigZombie.class}));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+        this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityVillager.class, false));
+        this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityIronGolem.class, true));
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn) {
+        if (super.attackEntityAsMob(entityIn)) {
+            if (entityIn instanceof EntityLivingBase) {
+                // This zombie gives health boost and regeneration when it attacks
+                ((EntityLivingBase)entityIn).addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 200));
+                ((EntityLivingBase)entityIn).addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 200));
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return LOOT;
+    }
+
+    @Override
+    protected boolean isValidLightLevel() {
+        return true;
+    }
+
+    @Override
+    public int getMaxSpawnedInChunk() {
+        return 5;
     }
 }
