6b283232385b22a46d4e0261d84a44e06b1edfef
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 10:35:38 +0300

    Create RenderHandlerCentaur.java

---
+++ src/main/java/com/nullptr/mod/util/handlers/RenderHandlerCentaur.java
@@ -0,0 +1,31 @@
+package com.nullptr.mod.util.handlers;
+
+import com.nullptr.mod.entity.centaur.EntityCentaur;
+import com.nullptr.mod.init.BlockInit;
+import com.nullptr.mod.entity.centaur.RenderCentaur;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+
+public class RenderHandler 
+{
+	public static void registerEntityRenders()
+	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityCentaur.class, new IRenderFactory<EntityCentaur>()
+		{
+			@Override
+			public Render<? super EntityCentaur> createRenderFor(RenderManager manager) 
+			{
+				return new RenderCentaur(manager);
+			}
+		});
+	}
+}
