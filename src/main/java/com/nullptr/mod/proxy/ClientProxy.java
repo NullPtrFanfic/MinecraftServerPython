bb0755d1ad68c046f49c8738fef249bbe89b43c3
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 13:16:17 +0300

    Update ClientProxy.java

--- src/main/java/com/nullptr/mod/proxy/ClientProxy.java
+++ src/main/java/com/nullptr/mod/proxy/ClientProxy.java
@@ -3,8 +3,30 @@ package com.nullptr.mod.proxy;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
+    @Override
+    public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
+
+        OBJLoader.INSTANCE.addDomain(ModTut.MODID);
+        ModelLoaderRegistry.registerLoader(new BakedModelLoader());
+
+        // Typically initialization of models and such goes here:
+        ModEntities.initModels();
+    }
+
     @Override
     public void registerItemRenderer(Item item, int meta, String id) {
         ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
