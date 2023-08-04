35b0c19bd0b1f64a84bd929aedc8cfa3a218d994
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 09:18:38 +0300

    Update SoundsHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/SoundsHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/SoundsHandler.java
@@ -11,12 +11,16 @@ public class SoundsHandler
 	public static SoundEvent TEST_AMBIENT;
 	public static SoundEvent TEST_HURT;
 	public static SoundEvent TEST_DEATH;
+	public static SoundEvent ENTITY_CENTAUR_AMBIENT, ENTITY_CENTAUR_HURT, ENTITY_CENTAUR_DEATH;
 	
 	public static void registerSounds()
 	{
 		TEST_AMBIENT = registerSound("entity.test.test_ambient");
 		TEST_HURT = registerSound("entity.test.test_hurt");
 		TEST_DEATH = registerSound("entity.test.test_death");
+		ENTITY_CENTAUR_AMBIENT = registerSound("entity.centaur.ambient");
+		ENTITY_CENTAUR_HURT = registerSound("entity.centaur.hurt");
+		ENTITY_CENTAUR_DEATH = registerSound("entity.centaur.death");
 	}
 	
 	private static SoundEvent registerSound(String name)
