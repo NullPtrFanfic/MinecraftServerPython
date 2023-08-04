63e3a6f48c0f811206ca0451d223ed55823946f2
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 16:51:49 +0300

    Update ModelRender.java

--- src/main/java/com/nullptr/mod/model/ModelRender.java
+++ src/main/java/com/nullptr/mod/model/ModelRender.java
@@ -11,26 +11,26 @@ import com.nullptr.mod.Main;
 public class ModelRender extends Render<Netero>{
 	  public static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID + ":textures/entities/pokemon/enderman.png");
 
-	  private ModelNetero model = new P25ModelPikachu();
+	  private ModelNetero model = new ModelNetero();
 	  @Override
-	  protected ResourceLocation getEntityTexture(P25Pikachu entity) {
-		  return TEXTURES;
-  	}
+	  protected ResourceLocation getEntityTexture(Netero entity) {
+	      return TEXTURES;
+  	  }
 
 	  public RenderNetero(RenderManager renderManager) {
 		    super(renderManager);
 	  }
 
-    @Override
-    public void doRender(Netero entity, double x, double y, double z, float entityYaw, float partialTicks) {
-    	  GlStateManager.pushMatrix();
-	      GlStateManager.translate(x, y, z);
+          @Override
+          public void doRender(Netero entity, double x, double y, double z, float entityYaw, float partialTicks) {
+    	            GlStateManager.pushMatrix();
+	            GlStateManager.translate(x, y, z);
 		    GlStateManager.rotate(180, 1, 0, 0);
-        GlStateManager.rotate(entity.rotationPitch, 1, 0, 0);
+                    GlStateManager.rotate(entity.rotationPitch, 1, 0, 0);
 		    GlStateManager.translate(0, -1.5, 0);
 		    this.bindEntityTexture(entity);
 		    this.model.render(entity, 0.0F, 0.0F, -0.1F, entity.rotationYaw, entity.rotationPitch, 0.0625F);
-	    	GlStateManager.popMatrix();
+	    	    GlStateManager.popMatrix();
 		    super.doRender(entity, x, y, z, entityYaw, partialTicks);
-    }
+          }
 }
