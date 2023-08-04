f141cde3c7ab187cbebed27a2db3619414ac3821
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 13:18:20 +0300

    Update CommonProxy.java

--- src/main/java/com/nullptr/mod/proxy/CommonProxy.java
+++ src/main/java/com/nullptr/mod/proxy/CommonProxy.java
@@ -1,8 +1,17 @@
 package com.nullptr.mod.proxy;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent e) {
+         ModEntities.init();
+    }
     public void registerItemRenderer(Item item, int meta, String id) {
         // Здесь ваш код
     }
