79c6f2666178362b1d91b2e3f8414c0d22b24d3f
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 12:10:31 +0300

    Update RenderHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
@@ -1,25 +1,17 @@
 package com.nullptr.mod.util.handlers;
 
 import com.nullptr.mod.entity.centaur.EntityCentaur;
-//import com.nullptr.mod.init.BlockInit;
 import com.nullptr.mod.entity.centaur.RenderCentaur;
 import com.nullptr.mod.entity.test.EntityTest;
 import com.nullptr.mod.entity.test.RenderTest;
-//import net.minecraft.block.state.IBlockState;
-//import net.minecraft.client.renderer.ItemMeshDefinition;
-//import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-//import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
-//import net.minecraft.item.Item;
-//import net.minecraft.item.ItemStack;
-//import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@OnlyIn(Dist.CLIENT)
+@SideOnly(Side.CLIENT)
 public class RenderHandler 
 {
 	public static void registerEntityRenders()
