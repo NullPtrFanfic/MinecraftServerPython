a22bd4f6de51568cd9fbfc3b1d5ea9c21b19b9ef
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 21:24:56 +0300

    Update ModelRender.java

--- src/main/java/com/nullptr/mod/model/ModelRender.java
+++ src/main/java/com/nullptr/mod/model/ModelRender.java
@@ -8,7 +8,51 @@ import com.nullptr.mod.model.Netero;
 import com.nullptr.mod.model.ModelNetero;
 import com.nullptr.mod.Main;
 
-public class ModelRender extends Render<Netero>{
+@SideOnly(Side.CLIENT)
+public class ModelRender extends Render<Netero> implements LayerRenderer<AbstractClientPlayer> {
+    static private IModel model;
+    static private IBakedModel bakedModel;
+    static ResourceLocation texture;
+
+    public ElementItemRender() {
+        this.texture = new ResourceLocation(References.MOD_ID, "models/item/tutorial_item.obj");
+        try {
+            this.model = OBJLoader.INSTANCE.loadModel(this.texture);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (this.model != null) {
+            this.bakedModel = this.model.bake(TRSRTransformation.identity(), DefaultVertexFormats.ITEM, ModelLoader.defaultTextureGetter());
+        }
+    }
+
+    private static void renderModel(IBakedModel model, VertexFormat fmt) {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder worldrenderer = tessellator.getBuffer();
+        worldrenderer.begin(GL11.GL_QUADS, fmt);
+        for (BakedQuad bakedquad : model.getQuads(null, null, 0)) {
+            worldrenderer.addVertexData(bakedquad.getVertexData());
+            // net.minecraftforge.client.model.pipeline.LightUtil.renderQuadColor(worldrenderer, bakedquad, -1);
+        }
+        tessellator.draw();
+    }
+
+    @Override
+    public void doRenderLayer(AbstractClientPlayer entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(entitylivingbaseIn.posX, entitylivingbaseIn.posY, entitylivingbaseIn.posZ);
+        // GlStateManager.enableRescaleNormal();
+        GlStateManager.scale(scale * 1.0f, scale * 1.0f, scale * 1.0f);
+        // this.renderModel(this.bakedModel, DefaultVertexFormats.ITEM);
+        GlStateManager.popMatrix();
+    }
+
+    @Override
+    public boolean shouldCombineTextures() {
+        return true;
+    }
+}
+
 	  public static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID + ":textures/entity/netero/netero.png");
 
 	  private ModelNetero model = new ModelNetero();
