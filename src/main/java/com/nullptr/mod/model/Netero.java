e53402fce83c9477465dc3b12137d70450af1901
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 17:19:58 +0300

    Update Netero.java

--- src/main/java/com/nullptr/mod/model/Netero.java
+++ src/main/java/com/nullptr/mod/model/Netero.java
@@ -1,4 +1,4 @@
-package pixelmonMod.entities.pokemonEntities.pokemon;
+package com.nullptr.mod.model;
 
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityAgeable;
@@ -17,77 +17,41 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
-import pixelmonMod.itemANDblock.items.pokeballs.Pokeballs;
+//import pixelmonMod.itemANDblock.items.pokeballs.Pokeballs;
 
-public class P25Pikachu extends EntityCreature{
-
-
-
-	public P25Pikachu(World worldIn) {
-
+public class Netero extends EntityCreature{
+	public Netero(World worldIn) {
 		super(worldIn);
-
 		this.setSize(1f, 2.4f);
-
 	}
 
-	
-
 	@Override
-
 	protected void initEntityAI() {
-
 		this.tasks.addTask(0, new EntityAISwimming(this));
-
 		this.tasks.addTask(1, new EntityAIWanderAvoidWater(this, 1.8));
-
-		this.tasks.addTask(2, new EntityAITempt(this, 1.5, Pokeballs.POKEBALL, false));
-
+		//this.tasks.addTask(2, new EntityAITempt(this, 1.5, Pokeballs.POKEBALL, false));
 		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 6f));
-
 		//this.tasks.addTask(4, new EntityAIAvoidEntity<Entity>(this, EntityPlayer.class, 4f, 2.4, 2.4));
-
 	}
 
-	
-
 	@Override
-
 	protected void applyEntityAttributes() {
-
 		super.applyEntityAttributes();
-
 		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(4);
-
 		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25);
-
 	}
 
 	
-
 	@Override
-
 	public float getEyeHeight() {
-
 		return 2.2f;
-
 	}
 
 	
 
 	@Override
-
 	public void onDeath(DamageSource cause) {
-
-		dropItem(Pokeballs.POKEBALL, 1);
-
+		//dropItem(Pokeballs.POKEBALL, 1);
 		this.isDead = true;
-
 	}
-
-	
-
-
-
 }
-
