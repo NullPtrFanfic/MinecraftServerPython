1264ce7a6b85565037b268c261906c7450ff8cce
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 12:21:27 +0300

    Update RenderHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
@@ -11,26 +11,32 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class RenderHandler 
 {
-	public static void registerEntityRenders()
-	{
-		/*RenderingRegistry.registerEntityRenderingHandler(EntityCentaur.class, new IRenderFactory<EntityCentaur>()
-		{
-			@Override
-			public Render<? super EntityCentaur> createRenderFor(RenderManager manager) 
-			{
-				return new RenderCentaur(manager);
-			}
-		});*/
-		RenderingRegistry.registerEntityRenderingHandler(EntityTest.class, new IRenderFactory<EntityTest>()
-		{
-			@Override
-			public Render<? super EntityTest> createRenderFor(RenderManager manager) 
-			{
-				return new RenderTest(manager);		
-			}
-		});
-	}
+    @SideOnly(Side.CLIENT)
+    public static void registerEntityRenders()
+    {
+        if (isClientSide()) {
+            /*RenderingRegistry.registerEntityRenderingHandler(EntityCentaur.class, new IRenderFactory<EntityCentaur>()
+            {
+                @Override
+                public Render<? super EntityCentaur> createRenderFor(RenderManager manager) 
+                {
+                    return new RenderCentaur(manager);
+                }
+            });*/
+            RenderingRegistry.registerEntityRenderingHandler(EntityTest.class, new IRenderFactory<EntityTest>()
+            {
+                @Override
+                public Render<? super EntityTest> createRenderFor(RenderManager manager) 
+                {
+                    return new RenderTest(manager);		
+                }
+            });
+        }
+    }
+
+    private static boolean isClientSide() {
+        return Side.CLIENT == net.minecraftforge.fml.common.FMLCommonHandler.instance().getEffectiveSide();
+    }
 }
