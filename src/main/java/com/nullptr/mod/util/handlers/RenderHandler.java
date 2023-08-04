67f4ac3a4a0bdd06fb77837657e0c1eb04c437ec
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 10:41:48 +0300

    Update RenderHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
@@ -1,31 +1,34 @@
 package com.nullptr.mod.util.handlers;
 
-//import com.tutorialmod.turtywurty.entity.diamondArrow.EntityDiamondArrow;
-//import com.tutorialmod.turtywurty.entity.diamondArrow.RenderDiamondArrow;
+import com.nullptr.mod.entity.centaur.EntityCentaur;
+//import com.nullptr.mod.init.BlockInit;
+import com.nullptr.mod.entity.centaur.RenderCentaur;
 import com.nullptr.mod.entity.test.EntityTest;
 import com.nullptr.mod.entity.test.RenderTest;
-import com.nullptr.mod.init.BlockInit;
-import com.nullptr.mod.Main;
-
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.ItemMeshDefinition;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+//import net.minecraft.block.state.IBlockState;
+//import net.minecraft.client.renderer.ItemMeshDefinition;
+//import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+//import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.client.model.ModelLoader;
+//import net.minecraft.item.Item;
+//import net.minecraft.item.ItemStack;
+//import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 public class RenderHandler 
 {
 	public static void registerEntityRenders()
 	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityCentaur.class, new IRenderFactory<EntityCentaur>()
+		{
+			@Override
+			public Render<? super EntityCentaur> createRenderFor(RenderManager manager) 
+			{
+				return new RenderCentaur(manager);
+			}
+		});
 		RenderingRegistry.registerEntityRenderingHandler(EntityTest.class, new IRenderFactory<EntityTest>()
 		{
 			@Override
@@ -35,4 +38,4 @@ public class RenderHandler
 			}
 		});
 	}
-  }
+}
