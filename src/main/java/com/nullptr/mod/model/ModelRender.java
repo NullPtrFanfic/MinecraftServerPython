d962e07d54db0924d90b93c602235ec901ee13f4
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 21:30:18 +0300

    Update ModelRender.java

--- src/main/java/com/nullptr/mod/model/ModelRender.java
+++ src/main/java/com/nullptr/mod/model/ModelRender.java
@@ -1,29 +1,37 @@
-package com.nullptr.mod.model;
+package com.nullptr.mod.client.renderer;
 
+import com.nullptr.mod.Main;
+import com.nullptr.mod.entity.Netero;
+import com.nullptr.mod.model.ModelNetero;
+
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexFormat;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
+import net.minecraft.client.renderer.vertex.VertexFormatElement.EnumUsage;
+import net.minecraft.client.renderer.vertex.VertexFormatElement.EnumType;
 import net.minecraft.util.ResourceLocation;
-import com.nullptr.mod.model.Netero;
-import com.nullptr.mod.model.ModelNetero;
-import com.nullptr.mod.Main;
+import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraftforge.common.model.IModel;
+import net.minecraftforge.common.model.TRSRTransformation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class ModelRender extends Render<Netero> implements LayerRenderer<AbstractClientPlayer> {
+public class RenderNetero extends Render<Netero> implements LayerRenderer<AbstractClientPlayer> {
     static private IModel model;
     static private IBakedModel bakedModel;
     static ResourceLocation texture;
 
-    public ElementItemRender() {
-        this.texture = new ResourceLocation(References.MOD_ID, "models/item/tutorial_item.obj");
-        try {
-            this.model = OBJLoader.INSTANCE.loadModel(this.texture);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (this.model != null) {
-            this.bakedModel = this.model.bake(TRSRTransformation.identity(), DefaultVertexFormats.ITEM, ModelLoader.defaultTextureGetter());
-        }
+    public RenderNetero(RenderManager renderManager) {
+        super(renderManager);
+        this.model = ModelLoaderRegistry.getModelOrLogError(new ResourceLocation(Main.MODID, "models/item/tutorial_item.obj"), "Missing model");
+        this.bakedModel = this.model.bake(TRSRTransformation.identity(), DefaultVertexFormats.ITEM, ModelLoader.defaultTextureGetter());
     }
 
     private static void renderModel(IBakedModel model, VertexFormat fmt) {
@@ -51,30 +59,25 @@ public class ModelRender extends Render<Netero> implements LayerRenderer<Abstrac
     public boolean shouldCombineTextures() {
         return true;
     }
-}
 
-	  public static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID + ":textures/entity/netero/netero.png");
+    public static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID + ":textures/entity/netero/netero.png");
+    private ModelNetero model = new ModelNetero();
 
-	  private ModelNetero model = new ModelNetero();
-	  @Override
-	  protected ResourceLocation getEntityTexture(Netero entity) {
-	      return TEXTURES;
-  	  }
+    @Override
+    protected ResourceLocation getEntityTexture(Netero entity) {
+        return TEXTURES;
+    }
 
-	  public ModelRender(RenderManager renderManager) {
-		    super(renderManager);
-	  }
-
-          @Override
-          public void doRender(Netero entity, double x, double y, double z, float entityYaw, float partialTicks) {
-    	            GlStateManager.pushMatrix();
-	            GlStateManager.translate(x, y, z);
-		    GlStateManager.rotate(180, 1, 0, 0);
-                    GlStateManager.rotate(entity.rotationPitch, 1, 0, 0);
-		    GlStateManager.translate(0, -1.5, 0);
-		    this.bindEntityTexture(entity);
-		    this.model.render(entity, 0.0F, 0.0F, -0.1F, entity.rotationYaw, entity.rotationPitch, 0.0625F);
-	    	    GlStateManager.popMatrix();
-		    super.doRender(entity, x, y, z, entityYaw, partialTicks);
-          }
+    @Override
+    public void doRender(Netero entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
+        GlStateManager.rotate(90, 1, 0, 0);
+        GlStateManager.rotate(entity.rotationPitch, 1, 0, 0);
+        GlStateManager.translate(0, -1.5, 0);
+        this.bindEntityTexture(entity);
+        this.model.render(entity, 0.0F, 0.0F, -0.1F, entity.rotationYaw, entity.rotationPitch, 0.0625F);
+        GlStateManager.popMatrix();
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+    }
 }
