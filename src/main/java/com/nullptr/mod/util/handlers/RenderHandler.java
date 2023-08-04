88ad77b23c4371263b0c0ad32ab78f62f60e873f
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 11:10:54 +0300

    Update RenderHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/RenderHandler.java
@@ -16,7 +16,10 @@ import net.minecraft.client.renderer.entity.RenderManager;
 //import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class RenderHandler 
 {
 	public static void registerEntityRenders()
