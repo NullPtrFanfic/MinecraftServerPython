2880f972c4ecd30c933b1c0f6f74a7a4d8b4f4fd
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-04 14:06:13 +0300

    Update CommonProxy.java

--- src/main/java/com/nullptr/mod/proxy/CommonProxy.java
+++ src/main/java/com/nullptr/mod/proxy/CommonProxy.java
@@ -1,6 +1,6 @@
 package com.nullptr.mod.proxy;
 
-import com.nullptr.mod.entity.weirdzombie.EntityWeirdZombie;
+import com.nullptr.mod.entity.weirdzombie.EntityInit;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class CommonProxy {
     public void preInit(FMLPreInitializationEvent e) {
-         EntityWeirdZombie.init();
+         EntityInit.init();
     }
     public void registerItemRenderer(Item item, int meta, String id) {
         // Здесь ваш код
