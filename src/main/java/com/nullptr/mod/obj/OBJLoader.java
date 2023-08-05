0499f098c630ffe4d5fc16f48dbb41dac7fbc5dd
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 16:01:21 +0300

    Update OBJLoader.java

--- src/main/java/com/nullptr/mod/obj/OBJLoader.java
+++ src/main/java/com/nullptr/mod/obj/OBJLoader.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import com.nullptr.mod.obj.ObjObject;
+import net.minecraft.block.material.Material;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
