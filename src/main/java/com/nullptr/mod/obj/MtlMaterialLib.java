179ed37dbec8fd8919a1c76e04313fd8fb80ece7
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 16:09:28 +0300

    Update MtlMaterialLib.java

--- src/main/java/com/nullptr/mod/obj/MtlMaterialLib.java
+++ src/main/java/com/nullptr/mod/obj/MtlMaterialLib.java
@@ -1,6 +1,7 @@
 package com.nullptr.mod.obj;
 
 import com.nullptr.mod.Main;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.BufferUtils;
