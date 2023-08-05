df4a2a3c9771b360ee5445fd8de7443cdc46c981
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 10:07:20 +0300

    Delete src/main/java/com/nullptr/mod/core/tileentity directory

--- src/main/java/com/nullptr/mod/core/tileentity/TileEntityEquipment.java
+++
@@ -1,7 +0,0 @@
-package com.nullptr.mod.core.tileentity;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityEquipment extends TileEntity {
-
-}
--- src/main/java/com/nullptr/mod/core/tileentity/TileEntityEquipmentPart.java
+++
@@ -1,6 +0,0 @@
-package com.nullptr.mod.core.tileentity;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityEquipmentPart extends TileEntity {
-}
