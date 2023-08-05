2ed7c58811490533062e3216147d976dfaadd60c
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 16:16:34 +0300

    Update RenderProjectileSprite.java

--- src/main/java/com/nullptr/mod/renderer/RenderProjectileSprite.java
+++ src/main/java/com/nullptr/mod/renderer/RenderProjectileSprite.java
@@ -1,7 +1,7 @@
-package com.lycanitesmobs.client.renderer;
+package com.nullptr.mod.renderer;
 
-import com.lycanitesmobs.LycanitesMobs;
-import com.lycanitesmobs.core.entity.*;
+import com.nullptr.mod.Main;
+//import com.lycanitesmobs.core.entity.*;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
