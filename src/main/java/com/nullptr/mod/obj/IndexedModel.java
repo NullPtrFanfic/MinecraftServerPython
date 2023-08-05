2ef4ddcbcd3a301a9e08973eb343108d4f68d5c4
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 16:22:11 +0300

    Update IndexedModel.java

--- src/main/java/com/nullptr/mod/obj/IndexedModel.java
+++ src/main/java/com/nullptr/mod/obj/IndexedModel.java
@@ -3,7 +3,7 @@ package com.nullptr.mod.obj;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 import java.util.ArrayList;
-import com.nullptr.mod.obj.ObjLoader;
+import com.nullptr.mod.obj.ObJLoader;
 
 
 public class IndexedModel {
