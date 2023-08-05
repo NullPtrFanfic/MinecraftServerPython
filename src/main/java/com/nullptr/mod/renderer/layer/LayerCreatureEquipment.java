79d0c4e44122b1ed7a339af756d9ed6ce0cc59e1
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 12:21:08 +0300

    Add files via upload

---
+++ src/main/java/com/nullptr/mod/renderer/layer/LayerCreatureEquipment.java
@@ -0,0 +1,40 @@
+package com.lycanitesmobs.client.renderer.layer;
+
+import com.lycanitesmobs.core.entity.BaseCreatureEntity;
+import com.lycanitesmobs.client.renderer.RenderCreature;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class LayerCreatureEquipment extends LayerCreatureBase {
+    public String equipmentSlot;
+
+    // ==================================================
+    //                   Constructor
+    // ==================================================
+    public LayerCreatureEquipment(RenderCreature renderer, String equipmentSlot) {
+        super(renderer);
+        this.equipmentSlot = equipmentSlot;
+    }
+
+
+    // ==================================================
+    //                  Render Layer
+    // ==================================================
+    @Override
+    public boolean canRenderLayer(BaseCreatureEntity entity, float scale) {
+        if(!super.canRenderLayer(entity, scale) || this.equipmentSlot == null)
+            return false;
+        return entity.getEquipmentName(this.equipmentSlot) != null;
+    }
+
+
+    // ==================================================
+    //                      Visuals
+    // ==================================================
+    @Override
+    public ResourceLocation getLayerTexture(BaseCreatureEntity entity) {
+        return entity.getEquipmentTexture(entity.getEquipmentName(this.equipmentSlot));
+    }
+}
