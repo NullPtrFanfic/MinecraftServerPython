ad6d2caf5e92068a22892708f7bfc6325a01716e
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-05 16:41:39 +0300

    Update RenderFactoryCreature.java

--- src/main/java/com/nullptr/mod/renderer/RenderFactoryCreature.java
+++ src/main/java/com/nullptr/mod/renderer/RenderFactoryCreature.java
@@ -1,28 +1,22 @@
-package com.lycanitesmobs.client.renderer;
+package com.nullptr.mod.renderer;
 
-import com.lycanitesmobs.LycanitesMobs;
-import com.lycanitesmobs.core.info.CreatureInfo;
+import com.nullptr.mod.Main;
+import com.nullpte.mod.renderer.RenderCreature;
+//import com.lycanitesmobs.core.info.CreatureInfo;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 public class RenderFactoryCreature<T extends Entity> implements IRenderFactory {
-    protected CreatureInfo creatureInfo;
+    //protected CreatureInfo creatureInfo;
 
-    public RenderFactoryCreature(CreatureInfo creatureInfo) {
-        this.creatureInfo = creatureInfo;
+    public RenderFactoryCreature(/*CreatureInfo creatureInfo*/) {
+       // this.creatureInfo = creatureInfo;
     }
 
     @Override
     public Render createRenderFor(RenderManager manager) {
-        try {
-            return new RenderCreature(this.creatureInfo.getName(), manager, (float) this.creatureInfo.width / 2);
-        }
-        catch (Exception e) {
-            LycanitesMobs.logWarning("", "An exception occurred rendering a creature model:");
-            e.printStackTrace();
-        }
         return null;
     }
 
