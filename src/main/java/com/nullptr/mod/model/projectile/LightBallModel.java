7a503ab0681056f95778cd7f14c7853361f13301
NullPtrFanfic <135633906+NullPtrFanfic@users.noreply.github.com>
2023-08-06 04:33:49 +0300

    Update LightBallModel.java

--- src/main/java/com/nullptr/mod/model/projectile/LightBallModel.java
+++ src/main/java/com/nullptr/mod/model/projectile/LightBallModel.java
@@ -29,7 +29,7 @@ public class LightBallModel extends ModelCreatureObj {
     public LightBallModel(float shadowSize) {
 
 		// Load Model:
-		this.initModel("lightball", "projectile/lightball");
+		this.initModel("pokeball", "projectile/lightball");
     }
 
 
