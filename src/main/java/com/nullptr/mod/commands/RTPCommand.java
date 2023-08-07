4130c78566260bfce52a406ecdaecc3797ba6587
NullPtrSumeru <135633906+NullPtrSumeru@users.noreply.github.com>
2023-08-07 07:47:12 +0300

    Create RTPCommand.java

---
+++ src/main/java/com/nullptr/mod/commands/RTPCommand.java
@@ -0,0 +1,36 @@
+package com.nullptr.mod.commands;
+
+import net.minecraft.command.*;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import java.util.Random;
+
+public class RTPCommand extends CommandBase {
+   @Override
+    public String getName() {
+        return "randomteleport";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/randomteleport - Teleports the player to a random location within 500 blocks.";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if (sender instanceof EntityPlayerMP) {
+            EntityPlayerMP player = getCommandSenderAsPlayer(sender);
+            Random random = new Random();
+            int range = 500;
+
+            double offsetX = random.nextInt(range * 2) - range;
+            double offsetZ = random.nextInt(range * 2) - range;
+
+            BlockPos newPos = new BlockPos(player.posX + offsetX, 60, player.posZ + offsetZ);
+            player.connection.setPlayerLocation(newPos.getX() + 0.5, newPos.getY(), newPos.getZ() + 0.5, player.rotationYaw, player.rotationPitch);
+
+            player.sendMessage(new TextComponentString("You have been randomly teleported."));
+        }
+    }
+}
