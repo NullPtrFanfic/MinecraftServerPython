ce36f616571a43dc85a4c24ad1959579960c2438
NullPtrSumeru <135633906+NullPtrSumeru@users.noreply.github.com>
2023-08-07 20:45:36 +0300

    Update ChatGPTBot.java

--- src/main/java/com/nullptr/mod/openai/ChatGPTBot.java
+++ src/main/java/com/nullptr/mod/openai/ChatGPTBot.java
@@ -2,18 +2,27 @@ package com.nullptr.mod.openai;
 
 import java.util.ArrayList;
 import net.minecraft.client.Minecraft;
+//import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentString;
+//import net.minecraft.util.IChatComponent;
 import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+//import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+//import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+//import com.theokanning.openai.completion.CompletionResponse;
 import com.theokanning.openai.service.OpenAiService;
+//import com.theokanning.openai.completion.chat.ChatMessage;
+//import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.completion.CompletionRequest;
 import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
-@Mod(modid = "openai_mod", version = "1.0")
 public class ChatGPTBot {
     public static final int MAX_MESSAGE_LENGTH = 2000;
     public static boolean gptEnabled = true;
@@ -28,18 +37,20 @@ public class ChatGPTBot {
                 e.printStackTrace();
             }
         }).exceptionally(throwable -> {
+            //getLogger().severe("Error while initializing OpenAI service! Is your API key valid?");
             throwable.printStackTrace();
             return null;
         });
     }
 
     public static String getResponse(String message) {
+        // Получение предыдущих сообщений пользователя
         ArrayList<String> inputs = new ArrayList<>();
 
         List<String> chatmessages = Minecraft.getMinecraft().ingameGUI.getChatGUI().getSentMessages();
         if (chatmessages.size() >= 5) {
-            int startIndex = Math.max(0, chatmessages.size() - 5);
-            int endIndex = Math.min(chatmessages.size(), startIndex + 5);
+            int startIndex = Math.max(0, chatmessages.size() - 5); // Получаем индекс первого сообщения в списке
+            int endIndex = Math.min(chatmessages.size(), startIndex + 5); // Получаем индекс последнего сообщения в списке
 
             if (endIndex > startIndex) {
                 for (int i = startIndex; i < endIndex; i++) {
@@ -54,31 +65,34 @@ public class ChatGPTBot {
             }
         }
 
+        // Далее можно продолжить обработку сообщений
+
         // Добавление текущего сообщения пользователя в запрос
         inputs.add(message + "\n");
         //final List<ChatMessage> messages = new ArrayList<>();
         //ChatMessage systemMessage;
         // Формирование текста запроса
-        StringBuilder request = new StringBuilder();
+        String request = "";
         for (String input : inputs) {
             // messages.add(new ChatMessage(ChatMessageRole.SYSTEM.value(), input););
-            request.append(input);
+            request+=input;
         }
-        return CompletableFuture.supplyAsync(() -> {
+        CompletableFuture<String> futureResponse = CompletableFuture.supplyAsync(() -> {
             try {
-                ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                ChatCompletionRequest chatCompletionRequest = api.createChatCompletion(ChatCompletionRequest.builder()
                         .model("gpt-3.5-turbo")
                         .temperature(0.8)
                         .maxTokens(MAX_MESSAGE_LENGTH)
                         // .messages(messages)
-                        .build();
-                CompletionRequest completionRequest = CompletionRequest.builder()
+                        .build());
+                CompletionRequest completionRequest = api.createChatCompletion(CompletionRequest.builder()
                         .model("ada")
-                        .prompt(request.toString())
+                        .prompt(request)
                         .echo(true)
                         .user("testing")
                         .n(3)
-                        .build();
+                        .build());
+                //CompletionResponse completionResponse = api.complete(completionRequest).get();
                 String response = api.createCompletion(completionRequest).getChoices().get(0).getText();
 
                 return response;
@@ -96,6 +110,7 @@ public class ChatGPTBot {
                 throw new RuntimeException(reason, throwable);
             }
             throw new RuntimeException(throwable);
-        }).join(); // Wait for the CompletableFuture to complete and return the result
+        });
+        return futureResponse.join(); // Wait for the CompletableFuture to complete and return the result
     }
 }
