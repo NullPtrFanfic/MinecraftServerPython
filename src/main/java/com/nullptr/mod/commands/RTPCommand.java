93154ea57fd3092a8d65d72b9c3ddb01494fd68b
NullPtrSumeru <135633906+NullPtrSumeru@users.noreply.github.com>
2023-08-07 07:57:57 +0300

    Update RTPCommand.java

--- src/main/java/com/nullptr/mod/commands/RTPCommand.java
+++ src/main/java/com/nullptr/mod/commands/RTPCommand.java
@@ -4,6 +4,7 @@ import net.minecraft.command.*;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import java.util.Random;
 
 public class RTPCommand extends CommandBase {
