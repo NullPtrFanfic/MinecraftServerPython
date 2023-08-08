3d86dbd7ab26208020f50fa87489720af43451e3
NullPtrSumeru <135633906+NullPtrSumeru@users.noreply.github.com>
2023-08-08 09:31:52 +0300

    Update ChatGPTBot.java

--- src/main/java/com/nullptr/mod/openai/ChatGPTBot.java
+++ src/main/java/com/nullptr/mod/openai/ChatGPTBot.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.time.Duration;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier; 
 import java.lang.Thread;
 
