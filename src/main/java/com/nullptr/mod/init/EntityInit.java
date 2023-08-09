016b2dd22b3c695689bee43d7225e79b6697961f
NullPtrSumeru <135633906+NullPtrSumeru@users.noreply.github.com>
2023-08-09 07:59:48 +0300

    Update EntityInit.java

--- src/main/java/com/nullptr/mod/init/EntityInit.java
+++ src/main/java/com/nullptr/mod/init/EntityInit.java
@@ -2,7 +2,7 @@ package com.nullptr.mod.init;
 import com.nullptr.mod.Main;
 import net.minecraft.entity.Entity;
 import com.nullptr.mod.entity.test.EntityTest;
-import com.nullptr.mod.model.Netero;
+//import com.nullptr.mod.model.Netero;
 import com.nullptr.mod.entity.centaur.EntityCentaur;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
@@ -12,7 +12,7 @@ public class EntityInit
    {
       registerEntity("test", EntityTest.class, Main.ENTITY_TEST, 50, 13310623, 65354);
       registerEntity("centaur", EntityCentaur.class, Main.ENTITY_CENTAUR, 50, 11437146, 000000);
-      registerEntity("Netero", Netero.class, Main.ENTITY_NETERO, 50, 16776960, 000000 );
+      //registerEntity("Netero", Netero.class, Main.ENTITY_NETERO, 50, 16776960, 000000 );
    }
    private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2)
    {
