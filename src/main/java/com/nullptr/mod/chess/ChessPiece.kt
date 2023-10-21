a372bd2e72a0adbf6ef8b3f23d2030d8ef7d786a
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-21 13:15:37 +0300

    Delete src/main/java/com/nullptr/mod/chess directory

--- src/main/java/com/nullptr/mod/chess/ChessPiece.kt
+++
@@ -1,8 +0,0 @@
-package com.nullptr.mod.chess
-
-import com.nullptr.mod.chess.ChessRank
-import com.nullptr.mod.chess.ChessPlayer
-
-data class ChessPiece(int col, int row, ChessPlayer player, ChessRank rank) {
-	
-}
--- src/main/java/com/nullptr/mod/chess/ChessPlayer.kt
+++
@@ -1,9 +0,0 @@
-package com.nullptr.mod.chess
-
-import com.nullptr.mod.chess.ChessPiece
-import com.nullptr.mod.chess.ChessRank
-
-enum class ChessPlayer {
-	WHITE,
-	BLACK
-}
