515ff11398e9a16c1b8bf6edcc9414a622619351
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-22 13:44:34 +0300

    Update SoundsHandler.java

--- src/main/java/com/nullptr/mod/util/handlers/SoundsHandler.java
+++ src/main/java/com/nullptr/mod/util/handlers/SoundsHandler.java
@@ -4,13 +4,15 @@ import com.nullptr.mod.util.SoundsUtil;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-i//mport net.minecraftforge.fml.common.registry.GameRegistry;
+//mport net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
+@EventBusSubscriber
 public class SoundsHandler {
     @SubscribeEvent
     public void registerSoundEvents(RegistryEvent.Register<SoundEvent> event) {
         IForgeRegistry<SoundEvent> registry = event.getRegistry();
-        registry.registerAll(SoundsUtil.SOUND_1.get(), SoundsUtil.SOUND_2.get());
+        registry.registerAll(SoundsUtil.SOUND_1.get(), SoundsUtil.SOUND_2.get(), SoundsUtil.SOUND_3.get());
     }
 }
