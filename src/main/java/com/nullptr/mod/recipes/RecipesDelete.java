d6562977e028ff4071e0e3329cf4aaef9c8b4920
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-22 16:45:45 +0300

    Update RecipesDelete.java

--- src/main/java/com/nullptr/mod/recipes/RecipesDelete.java
+++ src/main/java/com/nullptr/mod/recipes/RecipesDelete.java
@@ -3,10 +3,10 @@ package com.nullptr.mod.recipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
+//import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 import net.minecraft.util.ResourceLocation;
-
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 @EventBusSubscriber
 public class RecipesDelete {
  @SubscribeEvent 
