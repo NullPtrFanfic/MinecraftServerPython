c9046532a4e47579548bc62262a2840c13cd5a9f
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-22 16:35:14 +0300

    Create RecipesDelete.java

---
+++ src/main/java/com/nullptr/mod/recipes/RecipesDelete.java
@@ -0,0 +1,18 @@
+package com.nullptr.mod.recipes;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.registries.IForgeRegistryModifiable;
+import net.minecraft.util.ResourceLocation;
+
+@EventBusSubscriber
+public class RecipesDelete {
+ @SubscribeEvent 
+ public static void registerRecipes(RegistryEvent.Register event) { 
+        ResourceLocation WoodenAxe = new ResourceLocation("minecraft:wooden_axe");
+        IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
+        modRegistry.remove(WoodenAxe);
+ }
+}
