71ec238303c041a66ca1cc3b2fe176e77457a076
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-23 09:55:12 +0300

    Update EntityInit.java

--- src/main/java/com/nullptr/mod/init/EntityInit.java
+++ src/main/java/com/nullptr/mod/init/EntityInit.java
@@ -3,6 +3,7 @@ import com.nullptr.mod.Main;
 import net.minecraft.entity.Entity;
 import com.nullptr.mod.entity.test.EntityTest;
 //import com.nullptr.mod.model.Netero;
+import com.nullptr.mod.entity.python.EntityPython;
 import com.nullptr.mod.entity.centaur.EntityCentaur;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
@@ -12,7 +13,7 @@ public class EntityInit
    {
       registerEntity("test", EntityTest.class, Main.ENTITY_TEST, 50, 13310623, 65354);
       registerEntity("centaur", EntityCentaur.class, Main.ENTITY_CENTAUR, 50, 11437146, 000000);
-      //registerEntity("Netero", Netero.class, Main.ENTITY_NETERO, 50, 16776960, 000000 );
+      registerEntity("python", EntityPython.class, Main.ENTITY_PYTHON, 50, 16776960, 000000 );
    }
    private static void registerEntity(String name, Class<? extends Entity> entity, int id, int range, int color1, int color2)
    {
