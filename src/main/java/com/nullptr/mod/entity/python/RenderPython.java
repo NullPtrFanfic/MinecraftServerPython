f45971bf43af57e0640d11d50e394a5b27209f38
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-23 09:46:24 +0300

    Create RenderPython.java

---
+++ src/main/java/com/nullptr/mod/entity/python/RenderPython.java
@@ -0,0 +1,27 @@
+package com.nullptr.mod.entity.python;
+
+import com.nullptr.mod.Main;
+
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import com.nullptr.mod.entity.python.ModelPython;
+import com.nullptr.mod.entity.python.EntityPython;
+@SideOnly(Side.CLIENT)
+public class RenderPython extends RenderLiving<EntityPython>
+{
+	public static final ResourceLocation TEXTURE = new ResourceLocation(Main.MODID + ":textures/entity/test/test.png");
+	
+	public RenderTest(RenderManager manager) 
+	{
+		super(manager, new ModelPython(), 0.2f);
+	}
+	
+	@Override
+	protected ResourceLocation getEntityTexture(EntityPython entity) 
+	{
+		return TEXTURE;
+	}
+}
