c03e8ab8bf004dd8e717d09ad7692e40276f8f04
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 08:45:14 +0300

    Create EnumHandler.java

---
+++ src/main/java/com/nullptr/mod/util/handlers/EnumHandler.java
@@ -0,0 +1,11 @@
+package com.nullptr.mod.util.handlers;
+
+import net.minecraft.util.IStringSerializable;
+
+public class EnumHandler
+{
+   public static enum EnumType implements IStringSerializable
+   {
+      private static final
+   }
+}
