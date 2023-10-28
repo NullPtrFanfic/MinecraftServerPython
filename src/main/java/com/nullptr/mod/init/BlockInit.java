754bbfd317efd4fe196fff94a0f5dfafdd022926
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 16:36:26 +0300

    Update BlockInit.java

--- src/main/java/com/nullptr/mod/init/BlockInit.java
+++ src/main/java/com/nullptr/mod/init/BlockInit.java
@@ -7,11 +7,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import com.nullptr.mod.objects.blocks.BlockBase;
 import com.nullptr.mod.objects.blocks.BlockOres;
+import com.nullptr.mod.objects.blocks.BlockPlank;
+import com.nullptr.mod.objects.blocks.BlockLogs;
+import com.nullptr.mod.objects.blocks.BlockLeaf;
+import com.nullptr.mod.objects.blocks.BlockSaplings;
 public class BlockInit
 {
    public static final List<Block> BLOCKS = new ArrayList<Block>();
    public static final Block SILVER_BLOCK = new BlockBase("silver_block", Material.IRON);
   // public static final Block ORE_END = new BlockOres("ore_end", "end");
    public static final Block ORE_OVERWORLD = new BlockOres("ore_overworld", "overworld");
+   public static final Block PLANKS = new BlockPlank("planks");
+   public static final Block LOGS = new BlockLogs("planks");
+   public static final Block LEAVES = new BlockLeaf("planks");
+   public static final Block SAPLINGS = new BlockSaplings("planks");
   // public static final Block ORE_NETHER = new BlockOres("ore_nether", "nether");
 }
