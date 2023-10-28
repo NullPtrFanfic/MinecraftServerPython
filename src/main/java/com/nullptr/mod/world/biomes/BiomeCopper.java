20f382f772c0a939e466a369553197173ad714bd
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 17:01:31 +0300

    Update BiomeCopper.java

--- src/main/java/com/nullptr/mod/world/biomes/BiomeCopper.java
+++ src/main/java/com/nullptr/mod/world/biomes/BiomeCopper.java
@@ -10,8 +10,8 @@ public class BiomeCopper extends Biome
 	{
 		super(new BiomeProperties("Copper").setBaseHeight(1.0F).setHeightVariation(1.0F).setRainDisabled().setTemperature(1.0F));
 		
-		topBlock = BlockInit.COPPER_DIRT.getDefaultState();
-		fillerBlock = BlockInit.COPPER_ORE.getDefaultState();	
+		//topBlock = BlockInit.COPPER_DIRT.getDefaultState();
+	        fillerBlock = BlockInit.COPPER_ORE.getDefaultState();	
 		
 		this.spawnableCaveCreatureList.clear();
 		this.spawnableCreatureList.clear();
