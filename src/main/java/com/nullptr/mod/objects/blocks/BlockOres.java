b5dad67ac599276777b2263991c670562b0632a3
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 08:27:31 +0300

    Create BlockOres.java

---
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockOres.java
@@ -0,0 +1,14 @@
+package com.nullptr.mod.objects.blocks;
+
+import com.nullptr.mod.Main;
+
+public class BlockOres extends Block implements IHasModel, IMetaName
+{
+   public BlockOres(String name, Material material)
+   {
+      super(material);
+      setUnlocalizedName(name);
+      setRegistryName(name);
+      setCreativeTab
+   }
+}
