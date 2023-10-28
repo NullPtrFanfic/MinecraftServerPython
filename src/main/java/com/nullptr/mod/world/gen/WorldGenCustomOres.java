ffde071561526f920ceb4ce9b455fd116527aeab
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 13:10:30 +0300

    Update WorldGenCustomOres.java

--- src/main/java/com/nullptr/mod/world/gen/WorldGenCustomOres.java
+++ src/main/java/com/nullptr/mod/world/gen/WorldGenCustomOres.java
@@ -4,6 +4,7 @@ import com.nullptr.mod.objects.blocks.BlockOres;
 import com.nullptr.mod.util.handlers.EnumHandler;
 import net.minecraft.init.Blocks;
 import java.util.Random;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraft.world.chunk.IChunkProvider;
