08a30e2078dd7edb676c6a69bd6a39e689e795c6
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 19:31:54 +0300

    Update BlockInit.java

--- src/main/java/com/nullptr/mod/init/BlockInit.java
+++ src/main/java/com/nullptr/mod/init/BlockInit.java
@@ -7,19 +7,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import com.nullptr.mod.objects.blocks.BlockBase;
 import com.nullptr.mod.objects.blocks.BlockOres;
-import com.nullptr.mod.objects.blocks.BlockPlank;
-import com.nullptr.mod.objects.blocks.BlockLogs;
-import com.nullptr.mod.objects.blocks.BlockLeaf;
-import com.nullptr.mod.objects.blocks.BlockSaplings;
+//import com.nullptr.mod.objects.blocks.BlockPlank;
+import com.nullptr.mod.objects.blocks.BlockLogBase;
+import com.nullptr.mod.objects.blocks.BlockLeavesBase;
+import com.nullptr.mod.objects.blocks.BlockSaplingBase;
 public class BlockInit
 {
    public static final List<Block> BLOCKS = new ArrayList<Block>();
    public static final Block SILVER_BLOCK = new BlockBase("silver_block", Material.IRON);
   // public static final Block ORE_END = new BlockOres("ore_end", "end");
    public static final Block ORE_OVERWORLD = new BlockOres("ore_overworld", "overworld");
-   public static final Block PLANKS = new BlockPlank("planks");
-   public static final Block LOGS = new BlockLogs("planks");
-   public static final Block LEAVES = new BlockLeaf("planks");
-   public static final Block SAPLINGS = new BlockSaplings("planks");
+   public static final Block COPPER_LEAVES = new BlockLeavesBase("copper_leaves");
+	public static final Block COPPER_LOG = new BlockLogBase("copper_log");
+	public static final Block COPPER_SAPLING = new BlockSaplingBase("copper_sapling");
+	public static final Block ALUMINIUM_LEAVES = new BlockLeavesBase("aluminium_leaves");
+	public static final Block ALUMINIUM_LOG = new BlockLogBase("aluminium_log");
+	public static final Block ALUMINIUM_SAPLING = new BlockSaplingBase("aluminium_sapling");
   // public static final Block ORE_NETHER = new BlockOres("ore_nether", "nether");
 }
