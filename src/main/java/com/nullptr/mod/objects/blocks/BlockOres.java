7562d53f64f62a81f4978f032d2ee14975fe3ee1
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 08:40:05 +0300

    Update BlockOres.java

--- src/main/java/com/nullptr/mod/objects/blocks/BlockOres.java
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockOres.java
@@ -4,11 +4,21 @@ import com.nullptr.mod.Main;
 
 public class BlockOres extends Block implements IHasModel, IMetaName
 {
+   private String name, dimension;
    public BlockOres(String name, Material material)
    {
       super(material);
       setUnlocalizedName(name);
       setRegistryName(name);
-      setCreativeTab
+      setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+      this.name = name;
+      this.dimension = dimension;
+      BlockInit.BLOCKS.add(this);
+      ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+   }
+   @Override
+   public void registerModels()
+   {
+      Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
    }
 }
