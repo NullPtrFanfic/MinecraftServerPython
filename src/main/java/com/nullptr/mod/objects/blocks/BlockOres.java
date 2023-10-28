78e5cdb119118f6b4c3dc6bd3d679d993adca2ae
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-10-28 09:36:29 +0300

    Update BlockOres.java

--- src/main/java/com/nullptr/mod/objects/blocks/BlockOres.java
+++ src/main/java/com/nullptr/mod/objects/blocks/BlockOres.java
@@ -3,14 +3,17 @@ package com.nullptr.mod.objects.blocks;
 import com.nullptr.mod.Main;
 import com.nullptr.mod.objects.blocks.item.ItemBlockVariants;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import com.nullptr.mod.util.interfaces.IMetaName;
 import com.nullptr.mod.util.IHasModel;
 import com.nullptr.mod.util.handlers.EnumHandler;
 import com.nullptr.mod.init.ItemInit;
 import com.nullptr.mod.init.BlockInit;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.block.properties.PropertyEnum;
 public class BlockOres extends Block implements IHasModel, IMetaName
 {
-   public static final PropertyEnum<EnumHandler, EnumType> VARIANT = PropertyEnum.<EnumHandler.EnumType>create("variant", EnumHandler.EnumType.class);
+   public static final PropertyEnum<EnumHandler.EnumType> VARIANT = PropertyEnum.<EnumHandler.EnumType>create("variant", EnumHandler.EnumType.class);
    private String name, dimension;
    public BlockOres(String name, Material material)
    {
@@ -27,6 +30,9 @@ public class BlockOres extends Block implements IHasModel, IMetaName
    @Override
    public void registerModels()
    {
-      Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+      for(int i = 0; i < EnumHandler.EnumType.values().length; i++)
+      {
+          Main.proxy.registerVariantRenderer(Item.getItemFromBlock(this), i, "ore_" + this.dimension + EnumHandler.EnumType.values()[i].getName(), "inventory");
+      }
    }
 }
