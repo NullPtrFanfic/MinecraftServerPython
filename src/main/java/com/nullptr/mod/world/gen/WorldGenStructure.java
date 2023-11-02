9f79f398d8741d0b4c56bd5e0b282638bfacc339
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-02 08:37:30 +0200

    Update WorldGenStructure.java

--- src/main/java/com/nullptr/mod/world/gen/WorldGenStructure.java
+++ src/main/java/com/nullptr/mod/world/gen/WorldGenStructure.java
@@ -1,10 +1,16 @@
 package com.nullptr.mod.world.gen;
 
 import com.nullptr.mod.util.interfaces.IStructure;
+import com.nullptr.mod.Main;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.server.MinecraftServer;
 import java.util.Random;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.world.gen.structure.template.Template;
+import net.minecraft.world.gen.structure.template.TemplateManager;
 public class WorldGenStructure extends WorldGenerator
 {
    public static String structureName;
@@ -15,7 +21,20 @@ public class WorldGenStructure extends WorldGenerator
    @Override
    public boolean generate(World world, Random rand, BlockPos position)
    {
-      return false;
+      this.generateStructure(world, position);
+      return true;
    }
-   public static void generateStructure
+   public static void generateStructure(World world, BlockPos pos)
+   {
+      MinecraftServer mcServer = world.getMinecraftServer();
+      TemplateManager manager = worldServer.genStructureTemplateManager();
+      ResourceLocation location = new ResourceLocation(Main.MODID, structureName);
+      Template template = manager.get(mcServer, location);
+      if (template != null)
+      {
+          IBlockState state = world.getBlockState(pos);
+          world.notifyBlockUpdate(pos, state, state, 3);
+          template.addBlocksToWorldChunk(world, pos, settings);
+      }
+   }
 }
