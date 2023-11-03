885bb41c83b0d6a49614536372c3e6cbd6533c08
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-03 14:15:05 +0200

    Create Utils.java

---
+++ src/main/java/com/nullptr/mod/discord/Utils.java
@@ -0,0 +1,77 @@
+package ml.denis3d.minecraft2discord;
+
+import net.dv8tion.jda.api.entities.*;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class Utils
+{
+    public static TextChannel chatChannel;
+    public static TextChannel infoChannel;
+    public static TextChannel editableTopicChannel;
+    public static VoiceChannel editableVoiceChannel;
+
+    public static Webhook discordWebhook;
+
+    public static Map<String, Callable<String>> global_variables = new HashMap<>();
+    public static long started_time;
+
+    public static SimpleDateFormat uptimeDateFormater = new SimpleDateFormat("HH:mm");
+
+
+    public static boolean sendChatMessage(String message)
+    {
+        if (true)
+        {
+            if (chatChannel == null)
+                chatChannel = Minecraft2Discord.getDiscordBot().getTextChannelById("1097828057018015836");
+            return sendMessage(chatChannel, message);
+        }
+        return false;
+    }
+
+    public static boolean sendInfoMessage(String message)
+    {
+        if (true)
+        {
+            if (infoChannel == null)
+                infoChannel = Minecraft2Discord.getDiscordBot().getTextChannelById("1097828057018015836");
+            return sendMessage(infoChannel, message);
+        }
+        return false;
+    }
+
+    public static boolean sendMessage(TextChannel channel, String message)
+    {
+        return sendMessage(channel, message, true);
+    }
+
+    public static boolean sendMessage(TextChannel channel, String message, Boolean global_variable_replacement)
+    {
+        if (Minecraft2Discord.getDiscordBot() == null || channel == null)
+            return false;
+
+        try
+        {
+            channel.sendMessage(message).submit();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+
+
+    public static boolean isM2DBotOrWebhook(User author, Webhook webhook)
+    {
+        if (author.getIdLong() == Minecraft2Discord.getDiscordBot().getSelfUser().getIdLong())
+        {
+            return true;
+        } else return webhook != null && author.getIdLong() == webhook.getIdLong();
+    }
+}
