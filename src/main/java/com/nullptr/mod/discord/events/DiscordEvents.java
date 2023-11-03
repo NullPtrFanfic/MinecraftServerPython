96af947baa2a43d11f9c475f0a24404e4626de25
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-03 17:49:50 +0200

    Update DiscordEvents.java

--- src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
+++ src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
@@ -8,9 +8,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 //import net.minecraft.command.CommandSource;
 //import net.minecraft.command.Commands;
 
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -21,18 +18,6 @@ public class DiscordEvents extends ListenerAdapter
     public void onReady(ReadyEvent event)
     {
         System.out.println("Discord bot logged as " + event.getJDA().getSelfUser().getName());
-
-        Utils.global_variables.put("online_players", () -> String.valueOf(ServerLifecycleHooks.getCurrentServer().getCurrentPlayerCount()));
-        Utils.global_variables.put("max_players", () -> String.valueOf(ServerLifecycleHooks.getCurrentServer().getMaxPlayers()));
-        Utils.global_variables.put("unique_player", () -> String.valueOf(Optional.ofNullable(ServerLifecycleHooks.getCurrentServer().getWorld(DimensionType.OVERWORLD).getSaveHandler().getPlayerFolder().list()).map(list -> list.length).orElse(0)));
-        Utils.global_variables.put("motd", () -> ServerLifecycleHooks.getCurrentServer().getMOTD());
-        Utils.global_variables.put("mc_version", () -> ServerLifecycleHooks.getCurrentServer().getMinecraftVersion());
-        Utils.global_variables.put("server_hostname", () -> ServerLifecycleHooks.getCurrentServer().getServerHostname());
-        Utils.global_variables.put("server_port", () -> String.valueOf(ServerLifecycleHooks.getCurrentServer().getServerPort()));
-        Utils.global_variables.put("date", () -> DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now()));
-        Utils.global_variables.put("time", () -> DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
-        Utils.global_variables.put("uptime", () -> Utils.uptimeDateFormater.format(new Date().getTime() - Utils.started_time));
-
        // if (Config.SERVER.sendServerStartStopMessages.get())
         Utils.sendInfoMessage("Server started!");
     }
