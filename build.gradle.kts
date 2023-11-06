33cb262adb9ed206256c17f91a9509442882f822
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-06 16:28:30 +0200

    Update and rename build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,456 @@
+import net.minecraftforge.gradle.common.util.MinecraftExtension
+
+import net.minecraftforge.gradle.patcher.tasks.ReobfuscateJar
+
+import org.gradle.api.artifacts.type.ArtifactTypeDefinition
+
+import org.gradle.api.artifacts.transform.InputArtifact
+
+import org.gradle.api.artifacts.transform.TransformAction
+
+import org.gradle.api.artifacts.transform.TransformOutputs
+
+import org.gradle.api.artifacts.transform.TransformParameters
+
+import org.gradle.api.file.ArchiveOperations
+
+import org.gradle.api.file.FileSystemLocation
+
+import org.gradle.api.provider.Provider
+
+import java.text.SimpleDateFormat
+
+import java.util.*
+
+import java.util.jar.JarEntry
+
+import java.util.jar.JarInputStream
+
+import java.util.jar.JarOutputStream
+
+import javax.inject.Inject
+
+
+
+buildscript {
+
+    repositories {
+
+        maven(
+
+            url = "https://maven.minecraftforge.net/"
+
+        )
+
+        maven(
+
+            url = "https://mvnrepository.com/artifact/"
+
+        )
+
+        mavenCentral()
+
+        jcenter()
+
+        gradlePluginPortal()
+
+        mavenLocal()
+
+        maven(url = "https://jitpack.io")
+
+        maven(url = "https://libraries.minecraft.net")
+
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+
+        maven(url = "https://repo.spring.io/milestone")
+
+
+
+    }
+
+
+
+    dependencies {
+
+        classpath("net.minecraftforge.gradle:ForgeGradle:5.+")
+
+        classpath("com.github.jengelman.gradle.plugins:shadow:4.0.4")
+
+    }
+
+
+
+}
+
+
+
+plugins {
+
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+
+    kotlin("jvm") version "1.5.31"
+
+    id("java")
+
+    `maven-publish`
+
+}
+
+
+
+apply {
+
+    plugin("net.minecraftforge.gradle")
+
+    plugin("eclipse")
+
+    plugin("java")
+
+    plugin("com.github.johnrengelman.shadow")
+
+    plugin("maven-publish")
+
+}
+
+
+
+version = "0.1"
+
+group = "com.maha.mod"
+
+
+
+sourceSets {
+
+    main {
+
+        java {
+
+            srcDirs("src/main/java")
+
+            exclude("**/com/maha/mod/party/**")
+
+        }
+
+    }
+
+}
+
+
+
+
+
+val javaModuleAttribute = Attribute.of("javaModule", true.javaClass)
+
+
+
+configurations["compileClasspath"].attributes.attribute(javaModuleAttribute, true)
+
+configurations["runtimeClasspath"].attributes.attribute(javaModuleAttribute, true)
+
+
+
+dependencies.artifactTypes.maybeCreate("jar").attributes.attribute(javaModuleAttribute, false)
+
+
+
+dependencies.registerTransform(JavaModuleTransform::class.java) {
+
+    from.attributes.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, "jar")
+
+        .attribute(javaModuleAttribute, false)
+
+    to.attributes.attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, "jar").attribute(javaModuleAttribute, true)
+
+}
+
+
+
+val sourceCompatibility = JavaVersion.VERSION_1_8
+
+val targetCompatibility = JavaVersion.VERSION_1_8
+
+
+
+val Project.minecraft: MinecraftExtension
+
+    get() = extensions.getByType()
+
+
+
+dependencies {
+
+    minecraft("net.minecraftforge:forge:1.12.2-14.23.5.2860")
+
+    implementation("club.minnced:discord-webhooks:0.8.4")
+
+    implementation("org.ow2.asm:asm:7.1")
+
+    implementation("com.theokanning.openai-gpt3-java:service:0.12.0")
+
+    implementation( files ("JDA-4.4.1_353-withDependencies-no-opus.jar"))
+
+    implementation(gradleApi())
+
+    implementation("com.theokanning.openai-gpt3-java:service:0.12.0")
+
+    implementation("org.ow2.asm:asm:7.1")
+
+    implementation(files("JDA-4.4.1_353-withDependencies-no-opus.jar"))
+
+    shadow("org.ow2.asm:asm:7.1")
+
+    shadow("com.theokanning.openai-gpt3-java:service:0.12.0")
+
+    shadow( files ("JDA-4.4.1_353-withDependencies-no-opus.jar"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+
+}
+
+
+
+minecraft.run {
+
+    mappings("snapshot", "20171003-1.12")
+
+    runs {
+
+        create("client") {
+
+            workingDirectory(project.file("run"))
+
+        }
+
+        create("server") {
+
+        }
+
+    }
+
+}
+
+
+
+configurations {
+
+    shadow
+
+}
+
+
+
+java {
+
+    sourceCompatibility = JavaVersion.VERSION_1_9
+
+    targetCompatibility = JavaVersion.VERSION_1_9
+
+}
+
+
+
+tasks {
+
+    withType<Jar> {
+
+        archiveBaseName.set("mod")
+
+        manifest {
+
+            attributes(
+
+                hashMapOf(
+
+                    "Specification-Title" to "examplemod",
+
+                    "Specification-Vendor" to "examplemodsareus",
+
+                    "Specification-Version" to "1",
+
+                    "Implementation-Title" to project.name,
+
+                    "Implementation-Version" to "${version}",
+
+                    "Implementation-Vendor" to "examplemodsareus",
+
+                    "Implementation-Timestamp" to SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(Date())
+
+                )
+
+            )
+
+        }
+
+
+
+        finalizedBy("reobfShadowJar")
+
+    }
+
+    shadowJar {
+
+        configurations = mutableListOf(project.configurations.shadow.get())
+
+        relocate("org.ow2.asm", "${project.group}.shadow.org.objectweb.asm")
+
+        relocate("org.apache.commons.collections4", "${project.group}.shadow.org.apache.commons.collections4")
+
+        relocate("gnu.trove", "${project.group}.shadow.gnu.trove")
+
+        relocate("com.neovisionaries.ws.client", "${project.group}.shadow.com.neovisionaries.ws.client")
+
+        relocate("com.iwebpp.crypto", "${project.group}.shadow.com.iwebpp.crypto")
+
+        relocate("net.dv8tion.jda", "${project.group}.shadow.net.dv8tion.jda")
+
+        relocate("org.json", "${project.group}.shadow.org.json")
+
+        relocate("okio", "${project.group}.shadow.okio")
+
+        relocate("okhttp3", "${project.group}.shadow.okhttp3")
+
+        relocate("club.minnced.discord", "${project.group}.shadow.club.minnced.discord")
+
+        dependencies {
+
+            exclude(dependency("org.jetbrains:annotations"))
+
+            exclude(dependency("com.google.code.findbugs:jsr305"))
+
+        }
+
+        classifier = ""
+
+    }
+
+
+
+    withType<ReobfuscateJar> {
+
+        shadowJar {
+
+            
+
+        }
+
+    }
+
+}
+
+
+
+publishing {
+
+    publications {
+
+        register<MavenPublication>("maven") {
+
+            from(components["java"])
+
+        }
+
+    }
+
+    repositories {
+
+        maven(
+
+            url = "file:///${project.projectDir}/mcmodsrepo"
+
+        )
+
+    }
+
+}
+
+
+
+// @CacheableTransform - if transform results should also be shared via remote build cache
+
+abstract class JavaModuleTransform : TransformAction<TransformParameters.None> {
+
+
+
+    @get:InputArtifact
+
+    abstract val inputArtifact: Provider<FileSystemLocation>
+
+
+
+    @get:Inject
+
+    abstract val archiveOperations: ArchiveOperations
+
+
+
+    override fun transform(outputs: TransformOutputs) {
+
+        val isModule = archiveOperations.zipTree(inputArtifact).any { it.name == "module-info.class" }
+
+        if (isModule) {
+
+            outputs.file(inputArtifact)
+
+        } else {
+
+            val originalJar = inputArtifact.get().asFile
+
+            val target = outputs.file(originalJar.nameWithoutExtension + "-module.jar")
+
+
+
+            JarInputStream(originalJar.inputStream()).use { input ->
+
+                JarOutputStream(target.outputStream(), input.manifest).use { output ->
+
+                    var jarEntry = input.nextJarEntry
+
+                    while (jarEntry != null) {
+
+                        output.putNextEntry(jarEntry)
+
+                        output.write(input.readAllBytes())
+
+                        output.closeEntry()
+
+                        jarEntry = input.nextJarEntry
+
+                    }
+
+
+
+                    output.putNextEntry(JarEntry("module-info.class"))
+
+                    output.write(
+
+                        this::class.java.getResourceAsStream(
+
+                            "${originalJar.nameWithoutExtension}/module-info.class"
+
+                        )!!.readAllBytes()
+
+                    )
+
+                    output.closeEntry()
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+fun DependencyHandler.minecraft(
+
+    dependencyNotation: Any
+
+): Dependency = add("minecraft", dependencyNotation)!!
+
+
+
