8b5557bc0100b6c0e9a81548ce6b28fada71b5b0
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-09 19:24:37 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -213,8 +213,8 @@ dependencies {
 }
 minecraft {
    
-   // mappings("snapshot", "20171003-1.12")
-    mappings("stable", "39-1.12")
+    mappings("snapshot", "20171003-1.12")
+    //mappings("stable", "39-1.12")
 
     runs {
         val config = Action<RunConfig> {
