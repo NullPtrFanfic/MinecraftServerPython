989e79e789ad80fcd9624a86512222815c1deed4
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-12 05:32:11 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,10 +161,11 @@ configurations.all {
     }
 }
 configurations.all {
-    resolutionStrategy.eachDependency -> details {
+    resolutionStrategy.eachDependency { details -> {
         if (details.requested.group == "net.minecraftforge" && details.requested.name == "legacydev") {
             details.useVersion("0.2.3.1")
         }
+    }
     }
 }
 
