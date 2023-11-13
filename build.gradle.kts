a30c327f95d1a5ad5fd42a4b1859b3115ff55276
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-13 17:30:06 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.artifacts.DependencyResolveDetails
 import net.minecraftforge.gradle.patcher.tasks.ReobfuscateJar
 
 import org.gradle.api.artifacts.type.ArtifactTypeDefinition
-import com.nullptr.mod.JavaModuleTransform
+import src.main.com.nullptr.mod.JavaModuleTransform
 
 import java.text.SimpleDateFormat
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
