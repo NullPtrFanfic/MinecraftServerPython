b71c6787ed2c5ebe936451d63c24978136180def
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-15 08:26:29 +0200

    Update DiscordEvents.java

--- src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
+++ src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
@@ -20,9 +20,5 @@ public class DiscordEvents extends ListenerAdapter
         System.out.println("Discord bot logged as " + event.getJDA().getSelfUser().getName());
        // if (Config.SERVER.sendServerStartStopMessages.get())
         Utils.sendInfoMessage("Server started!");
-        WebhookClient.setDefaultErrorHandler((client, message, throwable) -> { 
-            System.err.printf("[%s] %s%n", client.getId(), message); 
-            if (throwable != null) throwable.printStackTrace(); // Shutdown the webhook client when you get 404 response (may also trigger for client#edit calls, be careful) 
-        });
     }
 }
