02a65e6e34e83dd9e6cb6398721a6d155bb001ea
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-15 03:23:13 +0200

    Update DiscordEvents.java

--- src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
+++ src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
@@ -2,7 +2,7 @@ package com.nullptr.mod.discord.events;
 
 
 import com.nullptr.mod.discord.Utils;
-
+import club.minnced.discord.webhook.WebhookClient;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 //import net.minecraft.command.CommandSource;
@@ -20,5 +20,12 @@ public class DiscordEvents extends ListenerAdapter
         System.out.println("Discord bot logged as " + event.getJDA().getSelfUser().getName());
        // if (Config.SERVER.sendServerStartStopMessages.get())
         Utils.sendInfoMessage("Server started!");
+        WebhookClient.setDefaultErrorHandler((client, message, throwable) -> { 
+            System.err.printf("[%s] %s%n", client.getId(), message); 
+            if (throwable != null) throwable.printStackTrace(); // Shutdown the webhook client when you get 404 response (may also trigger for client#edit calls, be careful) 
+            if (throwable instanceof HttpException ex && ex.getCode() == 404) { 
+                client.close();
+            } 
+        });
     }
 }
