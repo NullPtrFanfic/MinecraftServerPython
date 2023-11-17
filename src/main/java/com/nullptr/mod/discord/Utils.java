21feb141bc88cf63c696a09b3474d1c6ee5b7e2c
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-17 15:41:19 +0200

    Update Utils.java

--- src/main/java/com/nullptr/mod/discord/Utils.java
+++ src/main/java/com/nullptr/mod/discord/Utils.java
@@ -1,6 +1,6 @@
 package com.nullptr.mod.discord;
 
-import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.api.entities.*;
 import com.nullptr.mod.discord.Minecraft2Discord;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -9,10 +9,10 @@ import java.util.concurrent.Callable;
 
 public class Utils
 {
-    public static TextChannel chatChannel;
-    public static TextChannel infoChannel;
-    public static TextChannel editableTopicChannel;
-    public static VoiceChannel editableVoiceChannel;
+    public static MessageChannel chatChannel;
+    public static MessageChannel infoChannel;
+    public static MessageChannel editableTopicChannel;
+    //public static VoiceChannel editableVoiceChannel;
 
     public static Webhook discordWebhook;
 
@@ -46,12 +46,12 @@ public class Utils
         return false;
     }
 
-    public static boolean sendMessage(TextChannel channel, String message)
+    public static boolean sendMessage(MessageChannel channel, String message)
     {
         return sendMessage(channel, message, true);
     }
 
-    public static boolean sendMessage(TextChannel channel, String message, Boolean global_variable_replacement)
+    public static boolean sendMessage(MessageChannel channel, String message, Boolean global_variable_replacement)
     {
         if (Minecraft2Discord.getDiscordBot() == null || channel == null) {
             return false;
