2ab1f0699adc97672b4803d727f943e7c6435fc4
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-17 18:24:28 +0200

    Update Utils.java

--- src/main/java/com/nullptr/mod/discord/Utils.java
+++ src/main/java/com/nullptr/mod/discord/Utils.java
@@ -7,52 +7,17 @@ import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.WebhookClientBuilder;
+import club.minnced.discord.webhook.WebhookCluster;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
+import club.minnced.discord.webhook.send.WebhookEmbed;
+import okhttp3.OkHttpClient;
 
 public class Utils
 {
-    public static TextChannel chatChannel;
-    public static TextChannel infoChannel;
-    public static TextChannel editableTopicChannel;
-    //public static VoiceChannel editableVoiceChannel;
-
-    public static Webhook discordWebhook;
-
-    public static Map<String, Callable<String>> global_variables = new HashMap<>();
-    public static long started_time;
-
-    public static SimpleDateFormat uptimeDateFormater = new SimpleDateFormat("HH:mm");
-
-
-    public static boolean sendChatMessage(String message)
-    {
-        if (true)
-        {
-            if (chatChannel == null) {
-                chatChannel = Minecraft2Discord.getDiscordBot().getTextChannelById("1097828057018015836");
-            }
-            return sendMessage(chatChannel, message);
-        }
-        return false;
-    }
-
-    public static boolean sendInfoMessage(String message)
-    {
-        if (true)
-        {
-            if (infoChannel == null) {
-                infoChannel = Minecraft2Discord.getDiscordBot().getTextChannelById("1097828057018015836");
-            }
-            return sendMessage(infoChannel, message);
-        }
-        return false;
-    }
-
     public static boolean sendMessage(TextChannel channel, String message)
-    {
-        return sendMessage(channel, message, true);
-    }
-
-    public static boolean sendMessage(TextChannel channel, String message, Boolean global_variable_replacement)
     {
         if (Minecraft2Discord.getDiscordBot() == null || channel == null) {
             return false;
@@ -60,22 +25,51 @@ public class Utils
 
         try
         {
-            channel.sendMessage(message).submit();
+            List<Webhook> discordWebhooks = Utils.infoChannel.retrieveWebhooks().complete().stream()
+                            .filter(webhook -> webhook.getName().startsWith("mod")).collect(Collectors.toList());
+            if (discordWebhooks.size() == 0)
+            {
+                            Utils.discordWebhook = Utils.infoChannel.createWebhook("mod").complete();
+            } else
+            {
+                            Utils.discordWebhook = discordWebhooks.get(0);
+            }
+            // Using the builder
+            WebhookClientBuilder builder = new WebhookClientBuilder(Utils.discordWebhook.getUrl()); // or id, token
+            builder.setThreadFactory((job) -> {
+            Thread thread = new Thread(job);
+            thread.setName("Hello");
+            thread.setDaemon(true);
+            return thread;
+            });
+            builder.setWait(true);
+            ServerEvents.discordWebhookClient = builder.build();
+            // Using the factory methods
+            // Send and forget
+            ServerEvents.discordWebhookClient.send("Hello World");
+
+// Send and log (using embed)
+            WebhookEmbed embed = new WebhookEmbedBuilder()
+            .setColor(0xFF00EE)
+            .setDescription("Hello World")
+            .build();
+
+            ServerEvents.discordWebhookClient.send(embed)
+           .thenAccept((message) -> System.out.printf("Message with embed has been sent [%s]%n", message.getId()));
+
+// Change appearance of webhook message
+            WebhookMessageBuilder builder2 = new WebhookMessageBuilder();
+            builder2.setUsername("Minn"); // use this username
+            builder2.setAvatarUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzb7brumDODi9RhjQwxqILPKJKXK7UuLN2zXUbOAYMcurRF0RMV6Rxv7Fppa3K3gRv5Ek&usqp=CAU"); // use this avatar
+            builder2.setContent("Hello World");
+            ServerEvents.discordWebhookClient.send(builder2.build());
+            // Create and initialize the cluster
+            //sendWebhook(ServerEvents.discordWebhookClient);
         } catch (Exception e)
         {
             e.printStackTrace();
         }
 
         return true;
-    }
-
-
-
-    public static boolean isM2DBotOrWebhook(User author, Webhook webhook)
-    {
-        if (author.getIdLong() == Minecraft2Discord.getDiscordBot().getSelfUser().getIdLong())
-        {
-            return true;
-        } else return webhook != null && author.getIdLong() == webhook.getIdLong();
     }
 }
