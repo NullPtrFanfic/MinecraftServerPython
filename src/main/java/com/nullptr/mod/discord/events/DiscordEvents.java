ba6592547dda43332fb4bc83cb5e6f3c44dda871
NikitaFeaucher <86187498+NikitaFeaucher@users.noreply.github.com>
2023-11-17 18:18:45 +0200

    Update DiscordEvents.java

--- src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
+++ src/main/java/com/nullptr/mod/discord/events/DiscordEvents.java
@@ -1,26 +1,20 @@
 package com.nullptr.mod.discord.events;
 
 import com.nullptr.mod.discord.events.ServerEvents;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Webhook;
+//import net.dv8tion.jda.api.JDA;
+//import net.dv8tion.jda.api.entities.Webhook;
 //import club.minnced.discord.webhook.external.JDAWebhookClient;
 import com.nullptr.mod.discord.Utils;
 //import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 //import net.dv8tion.jda.api.utils.messages.MessageCreateData;
-import club.minnced.discord.webhook.WebhookClient;
+//import club.minnced.discord.webhook.WebhookClient;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 //import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 //import net.minecraft.command.CommandSource;
 //import net.minecraft.command.Commands;
-import club.minnced.discord.webhook.external.JDAWebhookClient;
-import club.minnced.discord.webhook.WebhookClient;
-import club.minnced.discord.webhook.WebhookClientBuilder;
-import club.minnced.discord.webhook.WebhookCluster;
-import club.minnced.discord.webhook.send.WebhookMessageBuilder;
-import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
-import club.minnced.discord.webhook.send.WebhookEmbed;
-import okhttp3.OkHttpClient;
+//import club.minnced.discord.webhook.external.JDAWebhookClient;
+
 //import com.mojang.brigadier.exceptions.CommandSyntaxException;
 //import com.nullptr.mod.discord.Config;
 import com.nullptr.mod.discord.Minecraft2Discord;
@@ -38,48 +32,5 @@ public class DiscordEvents extends ListenerAdapter
        // if (Config.SERVER.sendServerStartStopMessages.get())
         Utils.sendInfoMessage("Server started!");
         //Utils.chatChannel = Minecraft2Discord.getDiscordBot().getTextChannelById("1097828057018015836");
-        if (true)
-        {
-            List<Webhook> discordWebhooks = Utils.infoChannel.retrieveWebhooks().complete().stream()
-                            .filter(webhook -> webhook.getName().startsWith("mod")).collect(Collectors.toList());
-            if (discordWebhooks.size() == 0)
-            {
-                            Utils.discordWebhook = Utils.infoChannel.createWebhook("mod").complete();
-            } else
-            {
-                            Utils.discordWebhook = discordWebhooks.get(0);
-            }
-            // Using the builder
-            WebhookClientBuilder builder = new WebhookClientBuilder(Utils.discordWebhook.getUrl()); // or id, token
-            builder.setThreadFactory((job) -> {
-            Thread thread = new Thread(job);
-            thread.setName("Hello");
-            thread.setDaemon(true);
-            return thread;
-            });
-            builder.setWait(true);
-            ServerEvents.discordWebhookClient = builder.build();
-            // Using the factory methods
-            // Send and forget
-            ServerEvents.discordWebhookClient.send("Hello World");
-
-// Send and log (using embed)
-            WebhookEmbed embed = new WebhookEmbedBuilder()
-            .setColor(0xFF00EE)
-            .setDescription("Hello World")
-            .build();
-
-            ServerEvents.discordWebhookClient.send(embed)
-           .thenAccept((message) -> System.out.printf("Message with embed has been sent [%s]%n", message.getId()));
-
-// Change appearance of webhook message
-            WebhookMessageBuilder builder2 = new WebhookMessageBuilder();
-            builder2.setUsername("Minn"); // use this username
-            builder2.setAvatarUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzb7brumDODi9RhjQwxqILPKJKXK7UuLN2zXUbOAYMcurRF0RMV6Rxv7Fppa3K3gRv5Ek&usqp=CAU"); // use this avatar
-            builder2.setContent("Hello World");
-            ServerEvents.discordWebhookClient.send(builder2.build());
-            // Create and initialize the cluster
-            //sendWebhook(ServerEvents.discordWebhookClient);
-       }
     }
 }
